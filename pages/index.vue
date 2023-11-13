86c2255e86d06c96ff617fb3fb42044cedcf4e6f
Virtus-inc <aliondesh@gmail.com>
2023-11-13 10:33:58 +0200

    Moved the dropdown to a separate component

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,9 @@
 <template>
   <div class="flex justify-center items-center h-screen">
-    <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-      <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
-    </UDropdown>
+    <Dropdown />
   </div>
 </template>
 
 <script setup>
-const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }], [{
-    label: 'Edit',
-    icon: 'i-heroicons-pencil-square-20-solid',
-    shortcuts: ['E'],
-    click: () => {
-      console.log('Edit')
-    }
-  }, {
-    label: 'Make a copy',
-    icon: 'i-heroicons-document-duplicate-20-solid',
-    shortcuts: ['D'],
-    disabled: true
-  }], [{
-    label: 'Archive',
-    icon: 'i-heroicons-archive-box-20-solid'
-  }, {
-    label: 'Move',
-    icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-    label: 'Delete',
-    icon: 'i-heroicons-trash-20-solid',
-    shortcuts: ['⌘', 'D']
-  }]
-]
+  import Dropdown from "@/components/UI/Dropdown.vue"
 </script>
