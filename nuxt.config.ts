104c35c534568f7f6051129feffc9dea6765baef
Virtus-inc <aliondesh@gmail.com>
2023-11-13 10:14:05 +0200

    Replaced the current Headless UI with NuxtUI for a dropdown menu.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
+    '@nuxt/ui',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,91 +1,41 @@
 <template>
   <div class="lg:p-4">
     <p class="text-center lg:text-left">Fix me</p>
-    <div class="fixed lg:static top-100 lg:top-auto w-full text-left sm:text-center">
-      <Menu as="div" class="relative inline-block text-left">
-        <div>
-          <MenuButton
-              class="inline-flex w-full justify-center lg:justify-start rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-          >
-            Options
-          </MenuButton>
-        </div>
-
-        <transition
-          enter-active-class="transition duration-100 ease-out"
-          enter-from-class="transform scale-95 opacity-0"
-          enter-to-class="transform scale-100 opacity-100"
-          leave-active-class="transition duration-75 ease-in"
-          leave-from-class="transform scale-100 opacity-100"
-          leave-to-class="transform scale-95 opacity-0"
-        >
-          <MenuItems
-              class="absolute left-0 mt-2 w-48 sm:w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-          >
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  Account
-                </button>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  Edit
-                </button>
-              </MenuItem>
-            </div>
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  Make a copy
-                </button>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  Archive
-                </button>
-              </MenuItem>
-            </div>
-
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  Delete
-                </button>
-              </MenuItem>
-            </div>
-          </MenuItems>
-        </transition>
-      </Menu>
-    </div>
+    <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+      <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+    </UDropdown>
   </div>
 </template>
 
 <script setup>
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+const items = [
+  [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    }
+  }], [{
+    label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    shortcuts: ['E'],
+    click: () => {
+      console.log('Edit')
+    }
+  }, {
+    label: 'Make a copy',
+    icon: 'i-heroicons-document-duplicate-20-solid',
+    shortcuts: ['D'],
+    disabled: true
+  }], [{
+    label: 'Archive',
+    icon: 'i-heroicons-archive-box-20-solid'
+  }, {
+    label: 'Move',
+    icon: 'i-heroicons-arrow-right-circle-20-solid'
+  }], [{
+    label: 'Delete',
+    icon: 'i-heroicons-trash-20-solid',
+    shortcuts: ['⌘', 'D']
+  }]
+]
 </script>
