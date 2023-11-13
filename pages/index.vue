6c9eabe3fc6b50c9626dc51c0ef772770859e3c7
Virtus-inc <aliondesh@gmail.com>
2023-11-13 11:47:23 +0200

    Added heroicons to the dropdown menu items & Added dark mode toggle button

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
+  <div :class='data.status ? "dark" : "bg-white"' class="dark flex justify-center items-center h-screen">
     <Dropdown />
+    <UToggle v-model="data.status" color="green" @click="toggleTheme" class="ml-2"/>
   </div>
 </template>
 
 <script setup>
-  import Dropdown from "@/components/UI/Dropdown.vue"
+  import Dropdown from "~/components/iu/Dropdown.vue"
+
+  const data = ref({
+    status: false,
+  });
+
+  const toggleTheme = () => {
+    data.value.status = !data.value.status;
+  };
 </script>
